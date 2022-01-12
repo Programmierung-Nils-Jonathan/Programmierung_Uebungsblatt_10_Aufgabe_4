87779f9e967cf5979bc02e8c098b5525668c4a96
Nils Bollwerk <nil.bollwerk@gmail.com>
2022-01-12 22:07:27 +0000

    Aufgabe d) gemacht

--- Main.hs
+++ Main.hs
@@ -1,7 +1,8 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 import Text.Show.Functions
-data BinTree a b = 
+data BinTree a b =
             Empty
-            |Leaf b 
+            |Leaf b
             |Node (BinTree a b) a (BinTree a b)
             deriving Show
 
@@ -9,16 +10,23 @@ data BinTree a b =
 example :: BinTree (Int -> Bool) Char
 example = Node(Node(Leaf 'g') (\x->x*x==x) (Node(Leaf 'u') (\x->x==0) (Leaf 'l'))) (\x->x>4) (Node(Leaf 'f') (\x->x>=7) (Leaf 'i'))
 
-example2 :: BinTree Int Int 
+example2 :: BinTree Int Int
 example2 = Node (Leaf 124) 12 (Leaf 123)
 
-countInnerNodes :: BinTree a b -> Int 
+countInnerNodes :: BinTree a b -> Int
 countInnerNodes (Leaf e) = 0
 countInnerNodes (Node c f d)= 1 + countInnerNodes c + countInnerNodes d
 countInnerNodes Empty = 0
 
+decodeInt :: BinTree (Int -> Bool) b -> Int -> b
+decodeInt (Node x y z) f    | y(f)==True  =decodeInt z f
+                            | otherwise    =decodeInt x f
+decodeInt (Leaf d) f=d
+
+
+
+
 main :: IO()
-main = print (countInnerNodes example)
- 
-        
-    
+main = print (decodeInt example 10)
+
+
