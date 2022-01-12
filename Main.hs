555b16345e39f579a64b4f78dc686c52db8e1d3c
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2022-01-12 00:53:50 +0000

    started development

--- Main.hs
+++ Main.hs
@@ -1,1 +1,8 @@
+data BinTree a b = Bool (BinTree a b) (BinTree a b) | Char deriving Show
 
+example :: BinTree (Int -> Bool) Char
+example = BinTree (\x -> (x == 2)) "test"
+
+main = do
+    print("-------- a) --------")
+    print(example)
