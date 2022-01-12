d314c7089706047704e77a5840fc8a31b421f87b
Nils Bollwerk <nil.bollwerk@gmail.com>
2022-01-12 20:28:02 +0000

    commit

--- Main.hs
+++ Main.hs
@@ -1,8 +1,24 @@
-data BinTree a b = Bool (BinTree a b) (BinTree a b) | Char deriving Show
+import Text.Show.Functions
+data BinTree a b = 
+            Empty
+            |Leaf b 
+            |Node (BinTree a b) a (BinTree a b)
+            deriving Show
+
 
 example :: BinTree (Int -> Bool) Char
-example = BinTree (\x -> (x == 2)) "test"
+example = Node(Node(Leaf 'g') (\x->x*x==x) (Node(Leaf 'u') (\x->x==0) (Leaf 'l'))) (\x->x>4) (Node(Leaf 'f') (\x->x>=7) (Leaf 'i'))
 
-main = do
-    print("-------- a) --------")
-    print(example)
+example2 :: BinTree Int Int 
+example2 = Node (Leaf 124) 12 (Leaf 123)
+
+countInnerNodes :: BinTree a b -> Int 
+countInnerNodes (Leaf e) = 0
+countInnerNodes (Node c f d)= 1 + countInnerNodes c + countInnerNodes d
+countInnerNodes Empty = 0
+
+main :: IO()
+main = print (countInnerNodes example)
+ 
+        
+    
